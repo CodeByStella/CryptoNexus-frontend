ad60da2e4b255d4e71ca34bfdfdc7e15d1fc4272
smartdev <okovtun747@gmail.com>
2025-03-27 15:44:56 +0900

    update

--- src/services/socketService.ts
+++ src/services/socketService.ts
@@ -1,11 +1,13 @@
 import { io, Socket } from "socket.io-client";
 
+const API_BASE_URL = process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:5000";
+
 class SocketService {
   private socket: Socket;
   private static instance: SocketService;
 
   private constructor() {
-    this.socket = io("http://localhost:5000", { autoConnect: false });
+    this.socket = io(API_BASE_URL, { autoConnect: false });
   }
 
   public static getInstance(): SocketService {
